3511da626e75323a9e2f74fa82d347864621c7ef
JesMunRod <105549765+JesMunRod@users.noreply.github.com>
2022-06-22 21:04:14 -0500

    Add files via upload

---
+++ Preguntas_2_App.swift
@@ -0,0 +1,17 @@
+//
+//  Preguntas_2_App.swift
+//  Preguntas(2)
+//
+//  Created by Jesús Muñoz on 17/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Preguntas_2_App: App {
+    var body: some Scene {
+        WindowGroup {
+            WelcomeView()
+        }
+    }
+}
